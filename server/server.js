60a3dfc0185b282034f99378e0995f341e3e81bd
morelmiles <morelmiles@gmail.com>
2020-04-22 06:29:19 +0300

    Fectched the json from the API

--- server/server.js
+++ server/server.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const axios = require("axios");
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("<h1>Home page</h1>");
+});
+
+//Get the users from the  fake API
+app.get("/users", (req, res) => {
+  axios.get("https://randomuser.me/api/?page=1&results=10").then((response) => {
+    res.send(response.data);
+  });
 });
 
 //Listening to the app
